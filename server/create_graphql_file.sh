6fb30c6e0264e515197bafb2f8e7a713b1ac9602
naamoonoo <naamoonoo.y@gmail.com>
2020-01-07 02:11:29 +0100

    subscribe mode updated

--- server/create_graphql_file.sh
+++ server/create_graphql_file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$*" ]; then
- echo "put argument with FILE_NAME m:mutation/q:query format"
+ echo "put argument with FILE_NAME m:mutation/q:query/s:subcription format"
  exit 0
 
 fi
@@ -9,10 +9,19 @@ fi
 FILE_NAME=`echo $1 | cut -d '/' -f2`
 ROOT_FOLDER=`echo $1 | cut -d '/' -f1`
 
+
+if [ "$2" == "s" ]; then
+	echo `echo "type Subscription {
+		${FILE_NAME}:${ROOT_FOLDER}
+	}" > src/api/${ROOT_FOLDER}/${FILE_NAME}/${FILE_NAME}.graphql`
+	echo "src/api/${ROOT_FOLDER}/${FILE_NAME}/${FILE_NAME}.graphql successfully created"
+	exit 2
+fi
+
 if [ "$2" == "m" ]; then TYPE="Mutation"
 elif [ "$2" == "q" ]; then TYPE="Query"
 else
-	echo "put valid type m or q"
+	echo "put valid type m/q/s"
 	exit 1
 fi
 
--- server/create_resolvers_file.sh
+++ server/create_resolvers_file.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
 if [ -z "$*" ]; then
- echo "put argument with FILE_NAME m:mutation/q:query format"
+ echo "put argument with FILE_NAME m:mutation/q:query/s:subcription format"
  exit 0
 
 fi
 
 FILE_NAME=`echo $1 | cut -d '/' -f2`
 ROOT_FOLDER=`echo $1 | cut -d '/' -f1`
+
+
+if [ "$2" == "s" ]; then
+	echo `echo "import { withFilter } from 'graphql-yoga';
+
+	const resolvers = {
+		Subscription: {
+			${FILE_NAME}: {
+				subscribe: withFilter(
+					(_, __, { pubSub }) => {
+						return pubSub.asyncIterator();
+					},
+					(payload, _, { context }) => {}
+				)
+			}
+		}
+	};
+
+	export default resolvers;" > src/api/${ROOT_FOLDER}/${FILE_NAME}/${FILE_NAME}.resolvers.ts`
+	echo "src/api/${ROOT_FOLDER}/${FILE_NAME}/${FILE_NAME}.resolvers.ts successfully created"
+	exit 2
+fi
+
 
 if [ "$2" == "m" ]; then
 	TYPE="Mutation"
@@ -16,11 +39,11 @@ elif [ "$2" == "q" ]; then
 	TYPE="Query"
 	ARGS="__"
 else
-	echo "put valid type m or q"
+	echo "put valid type m/q/s"
 	exit 1
 fi
 
-echo `echo "import { Resolvers } from "../../../types/resolvers";
+echo `echo "import { Resolvers } from '../../../types/resolvers';
 
 const resolvers: Resolvers = {
 	${TYPE}: {
